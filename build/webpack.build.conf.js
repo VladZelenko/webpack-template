7b6611588f76a5a93af10925d46b08c455170deb
Vlad6555 <Vlad6555@ukr.net>
2019-06-27 11:15:03 +0300

    new version

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,10 +1,12 @@
 const merge = require('webpack-merge')
-const baseWebpackConfig = require('./webpack.base.config')
+const baseWebpackConfig = require('./webpack.base.conf')
+
 const buildWebpackConfig = merge(baseWebpackConfig, {
-    mode: 'production',
-    plugin: []
+  // BUILD config
+  mode: 'production',
+  plugins: []
 })
 
 module.exports = new Promise((resolve, reject) => {
-    resolve(buildWebpackConfig)
+  resolve(buildWebpackConfig)
 })
--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-import './js/common'
-import './css/main.css'
-import './scss/main.scss'
-window.Vue = require('vue')
+// JS
+import './js/'
+
+// SCSS
+import './assets/scss/main.scss'
+
+// Vue.js
+// window.Vue = require('vue')
+//
+// // Vue components (for use in html)
+// Vue.component('example-component', require('./components/Example.vue').default)
+//
+// // Vue init
+// const app = new Vue({
+//   el: '#app'
+// })
